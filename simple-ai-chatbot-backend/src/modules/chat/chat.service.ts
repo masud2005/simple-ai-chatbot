b00d621435f73aa7405a722b76729d534666380e
masud2005 <masud20012005@gmail.com>
2026-06-24 20:34:53 +0600

    fully completed AI chatbot

--- simple-ai-chatbot-backend/src/modules/chat/chat.service.ts
+++ simple-ai-chatbot-backend/src/modules/chat/chat.service.ts
@@ -8,9 +8,13 @@ export class ChatService {
   constructor(
     private prisma: PrismaService,
     private ai: AiService,
-  ) {}
+  ) { }
 
   async handleMessage(conversationId: string, content: string) {
+    const messageCount = await this.prisma.message.count({
+      where: { conversationId },
+    });
+
     // 1. save user message
     await this.prisma.message.create({
       data: {
@@ -19,6 +23,13 @@ export class ChatService {
         role: MessageRole.USER,
       },
     });
+
+    if (messageCount === 0) {
+      await this.prisma.conversation.update({
+        where: { id: conversationId },
+        data: { title: content.substring(0, 30) + (content.length > 30 ? '...' : '') },
+      });
+    }
 
     // 2. fake AI response (temporary)
     // const response = `AI: You said -> ${content}`;
